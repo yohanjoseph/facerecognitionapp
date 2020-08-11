c3698abbe45e18a194750a19dc4b3674a0b8b012
yohanjoseph <yohanjoseph@gmail.com>
2020-08-12 02:48:59 +0530

    deploy:

--- facerecognitionapp/src/App.js
+++ facerecognitionapp/src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
         this.state.input)
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://git.heroku.com/stormy-oasis-56133.git/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- facerecognitionapp/src/components/Register/Register.js
+++ facerecognitionapp/src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://git.heroku.com/stormy-oasis-56133.git/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
