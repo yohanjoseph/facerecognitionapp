bd6d64af75420a2aafe303c76b3e0efc610d98db
yohanjoseph <yohanjoseph@gmail.com>
2020-08-12 11:50:18 +0530

    Change url

--- facerecognitionapp/src/components/SignIn/Signin.js
+++ facerecognitionapp/src/components/SignIn/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    /*fetch('http://localhost:3000/signin', {
+    fetch('https://git.heroku.com/stormy-oasis-56133.git/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -32,8 +32,7 @@ class Signin extends React.Component {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
         }
-      })*/
-      this.props.onRouteChange('home');
+      })
   }
 
   render() {
